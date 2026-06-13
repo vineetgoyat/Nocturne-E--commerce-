128304a9270f8473b19a7dd79aa9ad3048201534
Vineet <vineetgoyat1@gmail.com>
2026-06-14 02:58:01 +0530

    Setup Nocturne homepage and routing foundation

--- NOCTURNE/client/src/main.jsx
+++ NOCTURNE/client/src/main.jsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter } from "react-router-dom";
+
 import "@fontsource/inter";
 import "@fontsource/cormorant-garamond";
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
