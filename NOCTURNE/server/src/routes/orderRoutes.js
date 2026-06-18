61c2f598c44618444dd71ed12ee383e5d31d10f9
Vineet <vineetgoyat1@gmail.com>
2026-06-19 03:36:19 +0530

    Phase 33 step 3

---
+++ NOCTURNE/server/src/routes/orderRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+import {
+  createOrder,
+  getOrders,
+} from "../controllers/orderController.js";
+
+const router = express.Router();
+
+router.post("/", createOrder);
+
+router.get("/", getOrders);
+
+export default router;
