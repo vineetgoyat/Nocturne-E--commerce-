f450610b5934b7c428fd2cbf86ced18f5f32370d
Vineet <vineetgoyat1@gmail.com>
2026-06-19 03:35:27 +0530

    Phase 33 step 2

---
+++ NOCTURNE/server/src/controllers/orderController.js
@@ -0,0 +1,27 @@
+import Order from "../models/orderModel.js";
+
+export const createOrder = async (req, res) => {
+  try {
+    const order = await Order.create(req.body);
+
+    res.status(201).json(order);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const getOrders = async (req, res) => {
+  try {
+    const orders = await Order.find().sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
