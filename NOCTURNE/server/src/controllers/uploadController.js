b540ce9c918254d1b4a76ecd3f9d3c06639c975c
Vineet <vineetgoyat1@gmail.com>
2026-06-18 21:03:39 +0530

    phase 26 starting its mistake of phase 20

--- NOCTURNE/server/src/controllers/uploadController.js
+++ NOCTURNE/server/src/controllers/uploadController.js
@@ -2,6 +2,8 @@ import cloudinary from "../config/cloudinary.js";
 
 export const uploadImage = async (req, res) => {
   try {
+    console.log(req.file);
+
     const result = await cloudinary.uploader.upload(
       req.file.path
     );
@@ -9,7 +11,12 @@ export const uploadImage = async (req, res) => {
     res.status(200).json({
       imageUrl: result.secure_url,
     });
+
   } catch (error) {
+
+    console.log("CLOUDINARY ERROR:");
+    console.log(error);
+
     res.status(500).json({
       message: error.message,
     });
--- NOCTURNE/server/src/server.js
+++ NOCTURNE/server/src/server.js
@@ -6,6 +6,9 @@ dotenv.config();
 
 console.log(process.env.CLOUDINARY_API_KEY);
 
+console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+
 const PORT = process.env.PORT || 8000;
 
 connectDB();
