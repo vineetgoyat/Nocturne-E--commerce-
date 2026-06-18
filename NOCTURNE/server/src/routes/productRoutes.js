216669109c2126c81d90aea78ba642521d01bb2a
Vineet <vineetgoyat1@gmail.com>
2026-06-18 21:06:41 +0530

    Phase 26 Step 4 completed - Cloudinary image upload fixed

--- NOCTURNE/server/src/routes/productRoutes.js
+++ NOCTURNE/server/src/routes/productRoutes.js
@@ -4,6 +4,8 @@ import {
   getProducts,
   getProductById,
   createProduct,
+  updateProduct,  
+  deleteProduct,
 } from "../controllers/productController.js";
 
 const router = express.Router();
@@ -13,5 +15,9 @@ router.get("/", getProducts);
 router.get("/:id", getProductById);
 
 router.post("/", createProduct);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
 
 export default router;
