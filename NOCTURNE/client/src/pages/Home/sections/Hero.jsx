90bcff69f44caf6174618e2849923a9153c21596
Vineet <vineetgoyat1@gmail.com>
2026-06-20 04:10:42 +0530

    adjusting spacing problem

--- NOCTURNE/client/src/pages/Home/sections/Hero.jsx
+++ NOCTURNE/client/src/pages/Home/sections/Hero.jsx
@@ -3,7 +3,7 @@ import Button from "../../../components/ui/Button";
 
 const Hero = () => {
   return (
-    <section className="relative min-h-screen flex items-center justify-center overflow-hidden bg-[#0A0A0A] text-white">
+    <section className="relative h-[85vh] flex items-center justify-center overflow-hidden bg-[#0A0A0A] text-white">
       {/* Glow */}
       <div className="absolute w-[500px] h-[500px] bg-yellow-500/10 blur-[150px] rounded-full" />
 
@@ -22,7 +22,13 @@ const Hero = () => {
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ delay: 0.2 }}
-          className="text-7xl md:text-9xl font-bold mt-6"
+          className="
+text-6xl
+md:text-8xl
+font-bold
+mt-6
+tracking-[8px]
+"
         >
           NOCTURNE
         </motion.h1>
@@ -31,7 +37,14 @@ const Hero = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.4 }}
-          className="max-w-2xl mx-auto mt-8 text-zinc-400 text-lg"
+          className="
+max-w-3xl
+mx-auto
+mt-8
+text-zinc-400
+text-xl
+leading-relaxed
+"
         >
           Enter a world where luxury is not purchased, it is discovered.
         </motion.p>
@@ -40,9 +53,12 @@ const Hero = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.6 }}
-          className="mt-10"
+          className="mt-14"
         >
           <Button>Enter The Collection</Button>
+          <p className="mt-10 text-zinc-600 tracking-[6px] text-xs uppercase">
+  Curated Luxury • Rare Artifacts • Timeless Legacy
+</p>
         </motion.div>
       </div>
     </section>
