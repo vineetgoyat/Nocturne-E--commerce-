a7bdd7f16d94cdc31815caac4865dd8351a2eb14
Vineet <vineetgoyat1@gmail.com>
2026-06-21 02:46:52 +0530

    redesigning the nocturne

--- NOCTURNE/client/src/components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="py-10 text-center border-t border-zinc-800">
-      <p>© 2026 NOCTURNE</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- NOCTURNE/client/src/services/productService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = "http://localhost:8000/api/products";
-
-export const fetchProducts = async () => {
-  const { data } = await axios.get(API);
-
-  return data;
-};
